87f1fb6735f4341754c78156b91858a3c0547b22
Will Maguire <will_maguire99@protonmail.com>
2026-06-02 20:55:24 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -22,9 +22,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer l.Close()
+	response := fmt.Sprint("HTTP/1.1 200 OK \r\n\r\n")
+	conn.Write([]byte(response))
+
 }
